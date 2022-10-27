7e587cb53064397d84dafb42d8d775c703e2cccb
Brett Tanner <bretttanner171@gmail.com>
2022-10-27 11:28:39 +0900

    Show created events on user#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
   devise_for :users
-  resources :events, only: [:index]
+  resources :events
 
   # Defines the root path route ("/")
   root "events#index"
