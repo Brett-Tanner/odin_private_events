ef154734b93bb2be23dc2989f4c9706e87eb0b62
Brett Tanner <bretttanner171@gmail.com>
2022-10-31 14:27:07 +0900

    Add ability to invite any user

--- app/controllers/participant_controller.rb
+++ app/controllers/participant_controller.rb
@@ -1,10 +1,11 @@
 class ParticipantController < ApplicationController
   def create
     event = Event.find(params[:event_id])
+    user = User.find(params[:id])
 
-    current_user.attended_events << event
+    user.attended_events << event
       
-    flash[:notice] = "Successfully signed up for this event"
+    flash[:notice] = "#{user.name} was invited"
     redirect_to event_path(event)
   end
 
@@ -14,7 +15,7 @@ class ParticipantController < ApplicationController
 
     user.attended_events.delete(event)
     
-    flash[:notice] = "#{user.name} is no longer attending this event"
+    flash[:notice] = "#{user.name} is no longer invited"
     redirect_to event_path(event)
   end
 end
